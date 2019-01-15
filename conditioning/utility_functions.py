6b361dac2e8a21c58795824e35c505bde2d7345d
Daniel Furman <furmandj@gmail.com>
2019-01-14 17:36:40 -0800

    Added some layers in discriminator, added examples of generated images

--- conditioning/utility_functions.py
+++ conditioning/utility_functions.py
@@ -44,7 +44,7 @@ def load_images(image_quantity):
 def face_check(img, bins):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    face_cascade = cv2.CascadeClassifier('/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('C:/Users/Daniel/Anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     if len(faces) != 1:
