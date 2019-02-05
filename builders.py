33e2acb6d9587eb78e39f3c9560c129735c5997b
Ben Price <price.benjamindaniel@gmail.com>
2019-02-04 18:22:45 -0800

    Moved new builders into `builders.py`. Updated other files.

--- builders.py
+++ builders.py
@@ -1,121 +1,174 @@
 from keras.models import Model
-from keras.layers import Input, Conv2D, UpSampling2D, Conv2DTranspose, \
-                         GlobalAveragePooling2D, concatenate, LeakyReLU, \
-                         BatchNormalization, Dense, Activation, Reshape
+from keras.layers import Input, Conv2D, UpSampling2D, Conv2DTranspose
+from keras.layers import GlobalAveragePooling2D, MaxPooling2D, concatenate
+from keras.layers import LeakyReLU, BatchNormalization, Dense, Activation, Reshape
 
-def build_generator(image_size):
-	# NOTE(ben): argument `image_size` is currently unused.
+from typing import List # Type hints requires Python >= 3.5
 
-    input1 = Input(shape=(None, None, 1))
-    
-    input11 = UpSampling2D()(input1)
-    
-    # [C L B C L B]
-    model1 = Conv2D(16, (3, 3), strides=(1, 1), padding='same')(input11)
-    model1 = LeakyReLU(alpha=0.2)(model1)
-    model1 = BatchNormalization(momentum=0.8)(model1)
-    model1 = Conv2D(16, (3, 3), strides=(1, 1), padding='same')(model1)
-    model1 = LeakyReLU(alpha=0.2)(model1)
-    model1 = BatchNormalization(momentum=0.8)(model1)
-    
-    # [C L B C L B C L B]
-    model2 = Conv2D(32, (3, 3), strides=(2, 2), padding='same')(model1)
-    model2 = LeakyReLU(alpha=0.2)(model2)
-    model2 = BatchNormalization(momentum=0.8)(model2)
-    model2 = Conv2D(32, (3, 3), strides=(1, 1), padding='same')(model2)
-    model2 = LeakyReLU(alpha=0.2)(model2)
-    model2 = BatchNormalization(momentum=0.8)(model2)
-    model2 = Conv2D(32, (3, 3), strides=(1, 1), padding='same')(model2)
-    model2 = LeakyReLU(alpha=0.2)(model2)
-    model2 = BatchNormalization(momentum=0.8)(model2)
+# Local imports
+from unet import unet_cell, LEAKY_RELU_ALPHA, BATCH_NORM_MOMENTUM
 
-    # [C L B C L B C L B]
-    model3 = Conv2D(64, (3, 3), strides=(2, 2), padding='same')(model2)
-    model3 = LeakyReLU(alpha=0.2)(model3)
-    model3 = BatchNormalization(momentum=0.8)(model3)
-    model3 = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(model3)
-    model3 = LeakyReLU(alpha=0.2)(model3)
-    model3 = BatchNormalization(momentum=0.8)(model3)
-    model3 = Conv2D(64, (3, 3), strides=(1, 1), padding='same')(model3)
-    model3 = LeakyReLU(alpha=0.2)(model3)
-    model3 = BatchNormalization(momentum=0.8)(model3)
+# Create a function for constructing the upscaler (aka generator)
+def build_upscaler(inputs, # TODO: What type is this?
+                   num_filters_in_layer: List[int]=[16, 32, 64],
+                   num_cells_in_layer: List[int]=[3, 3, 3],
+                   bottleneck_before_concat: bool=False) -> Model:
+    """Build a model which upscales inputs by factor of 4 (i.e., 
+       doubles the height and width of inputs)
+    
+    # Arguments
+        inputs (Tensor): images to be upscaled; must have 
+            data_format == 'channels_last' # TODO: Enable both formats
+            
+        num_filters_in_layer (list of ints): how many filters are 
+            used in each layer. (Note: a "layer" consists of
+            successive applications of stride=1 convolution; two 
+            layers are separated by a pooling or upsampling operation. 
+            The length of this list tells us how many layers we have.)
+            
+        num_cells_in_layer (list of ints): how many successive 
+            applications of convolution + BN + activation are applied 
+            before starting a new layer.
+            
+        bottleneck_before_concat (bool): whether or not to apply a 
+            convolution with 1x1 kernels prior to concatenating 
+            (doing so will reduce the total number of parameters 
+            required by the upscaler)
+    
+    # Returns
+        Keras Model which takes in an image tensor and returns 
+        a tensor of upscaled images.
+    """
+    
+    # Validate inputs
+    if len(num_filters_in_layer) != len(num_cells_in_layer):
+        raise ValueError("len(num_filters_in_layer) should equal "
+            "len(num_cells_in_layer). Received lengths: "
+            "({}, {})".format(len(num_filters_in_layer), len(num_cells_in_layer)))
+    
+    x = inputs
+    x = UpSampling2D()(x)
+    carry_forward_tensors = []
 
-    # [Conc L B C L B C L B]
-    model4 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(model3), model2], axis=3)
-    model4 = LeakyReLU(alpha=0.2)(model4)
-    model4 = BatchNormalization(momentum=0.8)(model4)
-    model4 = Conv2D(32, (3, 3), strides=(1, 1), padding='same')(model4)
-    model4 = LeakyReLU(alpha=0.2)(model4)
-    model4 = BatchNormalization(momentum=0.8)(model4)
-    model4 = Conv2D(32, (3, 3), strides=(1, 1), padding='same')(model4)
-    model4 = LeakyReLU(alpha=0.2)(model4)
-    model4 = BatchNormalization(momentum=0.8)(model4)
-    
-    # [Conc L B C L B C L B]
-    model5 = concatenate([Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(model4), model1], axis=3)
-    model5 = LeakyReLU(alpha=0.2)(model5)
-    model5 = BatchNormalization(momentum=0.8)(model5)
-    model5 = Conv2D(16, (3, 3), strides=(1, 1), padding='same')(model5)
-    model5 = LeakyReLU(alpha=0.2)(model5)
-    model5 = BatchNormalization(momentum=0.8)(model5)
-    model5 = Conv2D(16, (3, 3), strides=(1, 1), padding='same')(model5)
-    model5 = LeakyReLU(alpha=0.2)(model5)
-    model5 = BatchNormalization(momentum=0.8)(model5)
-    
-    # [Conc L B C_sigmoid]
-    model6 = concatenate([Conv2D(16, (1, 1), padding='same')(model5), input11], axis=3)
-    model6 = LeakyReLU(alpha=0.2)(model6)
-    model6 = BatchNormalization(momentum=0.8)(model6)
-    model6 = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(model6)
-    
-    return Model(inputs=input1, outputs=model6)
+    # Zip the parameters that apply to the encoder (exclude last 
+    # parameters for bottom layer)
+    contraction_filters_and_cells = zip(num_filters_in_layer[:-1],
+                                        num_cells_in_layer[:-1])
 
-def build_discriminator(image_size):
-    input1 = Input(shape=(None, None, 1))
-    input2 = Input(shape=(None, None, 1))
-    
-    model1 = UpSampling2D()(input1)
-    model1 = Conv2D(16, (3, 3), strides=(1,1), padding='same')(model1)
-    model1 = LeakyReLU(alpha=0.2)(model1)
-    model1 = BatchNormalization(momentum=0.8)(model1)
-    model1 = Conv2D(16, (3, 3), strides=(1,1), padding='same')(model1)
-    model1 = LeakyReLU(alpha=0.2)(model1)
-    model1 = BatchNormalization(momentum=0.8)(model1)
-    
-    model = Conv2D(16, (3, 3), strides=(1,1), padding='same')(input2)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Conv2D(16, (3, 3), strides=(1,1), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = concatenate([model, model1], axis=3)
-    
-    model = Conv2D(32, (3, 3), strides=(2,2), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Conv2D(64, (3, 3), strides=(1,1), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Conv2D(64, (3, 3), strides=(1,1), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    
-    model = Conv2D(128, (3, 3), strides=(2,2), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Conv2D(256, (3, 3), strides=(1,1), padding='same')(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Conv2D(256, (3, 3), strides=(1,1), padding='same')(model)
-    model = GlobalAveragePooling2D()(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Dense(1024)(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = BatchNormalization(momentum=0.8)(model)
-    model = Dense(512)(model)
-    model = LeakyReLU(alpha=0.2)(model)
-    model = Dense(1)(model)
-    model = Activation('sigmoid')(model)
+    # Encoder
+    for i, (num_filters, num_cells) in enumerate(contraction_filters_and_cells):
+        if i == 0:
+            # First layer does not downsample
+            for j in range(num_cells):
+                x = unet_cell(x, num_filters=num_filters) 
+                # Note: We make extensive use of default values.
+        else:
+            # Downsample first
+            x = unet_cell(x, num_filters=num_filters, strides=2)
+            for j in range(num_cells - 1):
+                x = unet_cell(x, num_filters=num_filters)
+        carry_forward_tensors.append(x)
 
-    return Model(inputs=[input1, input2], outputs=model)
+    # Bottom layer
+    num_filters_at_bottom = num_filters_in_layer[-1]
+    num_cells_at_bottom = num_cells_in_layer[-1]
+
+    x = unet_cell(x, num_filters=num_filters_at_bottom, strides=2)
+    for i in range(num_cells_at_bottom - 1):
+        x = unet_cell(x, num_filters=num_filters_at_bottom)
+
+    # Decoder
+    filters_cells_and_carrys = reversed(
+        list(zip(num_filters_in_layer[:-1],
+                 num_cells_in_layer[:-1],
+                 carry_forward_tensors)))
+    
+    for num_filters, num_cells, carry in filters_cells_and_carrys:
+        # First, we upsample the output of the previous layer
+        # TODO: parametrize upsampling method (Conv2DTrans vs UpSample2D)
+        x = unet_cell(x, transpose_conv=True, num_filters=num_filters, strides=2)
+        if bottleneck_before_concat:
+            x = unet_cell(x, num_filters=num_filters//2, kernel_size=1)
+            carry = unet_cell(carry, num_filters=num_filters//2, kernel_size=1)
+        x = concatenate([x, carry])
+        # Then, we apply successive convolution
+        for _ in range(num_cells - 1):
+            x = unet_cell(x, num_filters=num_filters)
+
+    # Output
+    x = unet_cell(x, num_filters=1, kernel_size=1, activation='sigmoid',
+                  batch_normalization=False)
+
+    model = Model(inputs=inputs, outputs=x)
+    return model
+
+# TODO: Test keras.applications (ResNet, Xception, etc.) as discriminators
+# Note: ResNet, Xception have *required* input dimensions larger than 
+# something on the order of 100x100...
+# Instead, consider implementing and using ResNet / Xception cells
+def build_discriminator(low_res_input, high_res_input, # TODO: What type are these?
+                        num_filters_in_layer: List[int]=[16, 32, 64],
+                        num_cells_in_layer: List[int]=[3, 3, 3],
+                        num_units_in_dense_layer: List[int]=[]):
+    """Builds a model which classifies high_res_input as real
+       or generated (where "real" -> 1, "generated" -> 0).
+    
+    # Arguments
+        low_res_input (Tensor): low resolution images
+        
+        high_res_input (Tensor): upsampled versions of low_res_input
+        
+        # Note: both input tensors should have data_format == 'channels_last'
+        # TODO: Enable both formats
+        
+        num_filters_in_layer (list of ints): how many filters are 
+            used in each layer. (Note: a "layer" consists of
+            successive applications of stride=1 convolution; two 
+            layers are separated by a pooling operation. The length 
+            of this list tells us how many layers we have.)
+            
+        num_cells_in_layer (list of ints): how many successive 
+            applications of convolution + BN + activation are applied 
+            before starting a new layer.
+            
+        num_units_in_dense_layer (list of ints): how many units are 
+            used in each dense layer (Note: the length of this list 
+            tells us how many dense layers we have before the final 
+            classification layer.)
+    
+    # Returns
+        Keras Model which takes in two image tensors (low_res_input, 
+        high_res_input) and returns a scalar between [0, 1] which 
+        represents how realistic high_res_input appears.
+    """
+    
+    # Validate inputs
+    if len(num_filters_in_layer) != len(num_cells_in_layer):
+        raise ValueError("len(num_filters_in_layer) should equal "
+            "len(num_cells_in_layer). Received lengths: "
+            "({}, {})".format(len(num_filters_in_layer), len(num_cells_in_layer)))
+    
+    x = low_res_input
+    x = UpSampling2D()(x)
+    x = concatenate([x, high_res_input])
+    
+    filters_and_cells = zip(num_filters_in_layer, num_cells_in_layer)
+    
+    for i, (num_filters, num_cells) in enumerate(filters_and_cells):
+        for j in range(num_cells):
+            x = unet_cell(x, num_filters=num_filters) # TODO: Rename `unet_cell`
+        x = MaxPooling2D(pool_size=2)(x)
+    
+    x = GlobalAveragePooling2D()(x)
+    x = LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x)
+    x = BatchNormalization(momentum=BATCH_NORM_MOMENTUM)(x)
+    
+    for num_units in num_units_in_dense_layer:
+        x = Dense(units=num_units)(x)
+        x = LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x)
+        x = BatchNormalization(momentum=BATCH_NORM_MOMENTUM)(x)
+    
+    validity = Dense(units=1, activation='sigmoid')(x)
+
+    model = Model(inputs=[low_res_input, high_res_input], outputs=validity)
+    return model
---
+++ utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def reduce_resolution(ims):
+    O, M, N, P = ims.shape
+    K = 2
+    L = 2
+
+    MK = M // K
+    NL = N // L
+    return ims.reshape(-1, MK, K, NL, L).mean(axis=(2, 4)).reshape(O, MK, NL, P)
